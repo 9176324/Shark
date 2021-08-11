dd631dd577e322454d7ee5ec5bc44293b2f70fe1
blindtiger <blindtiger@foxmail.com>
2021-08-11 11:01:31 +0800

    Wednesday 08/11/2021 11:01:31:833

--- Projects/Shark/PatchGuard.h
+++ Projects/Shark/PatchGuard.h
@@ -96,13 +96,13 @@ extern "C" {
 
 #define GetGpBlock(pgb) (pgb->GpBlock)
 
-#define PG_MAXIMUM_CONTEXT_COUNT 0x00000003UI32 // ���ܴ��ڵ� Context �������
-#define PG_FIRST_FIELD_OFFSET 0x00000100UI32 // ����ʹ�õĵ�һ�� Context ��Աƫ��
-#define PG_CMP_APPEND_DLL_SECTION_END 0x000000c0UI32 // CmpAppendDllSection ����
-#define PG_COMPARE_FIELDS_COUNT 0x00000004UI32 // ����ʱ�Ƚϵ� Context ��Ա����
-#define PG_COMPARE_BYTE_COUNT 0x00000010UI32 // ���� Worker ʱ�Ƚϵ��ֽ�����
+#define PG_MAXIMUM_CONTEXT_COUNT 0x00000003UI32 // 可能存在的 Context 最大数量
+#define PG_FIRST_FIELD_OFFSET 0x00000100UI32 // 搜索使用的第一个 Context 成员偏移
+#define PG_CMP_APPEND_DLL_SECTION_END 0x000000c0UI32 // CmpAppendDllSection 长度
+#define PG_COMPARE_FIELDS_COUNT 0x00000004UI32 // 搜索时比较的 Context 成员数量
+#define PG_COMPARE_BYTE_COUNT 0x00000010UI32 // 搜索 Worker 时比较的字节数量
 
-        // EntryPoint �����С ��������ͷ���Ĵ���Ƭ�� ( ��С���� =  max(2 * 8 + 7, sizeof(GUARD_BODY)) )
+		// EntryPoint 缓存大小 用来搜索头部的代码片段 ( 最小长度 =  max(2 * 8 + 7, sizeof(GUARD_BODY)) )
 #define PG_MAXIMUM_EP_BUFFER_COUNT ((max(2 * 8 + 7, sizeof(GUARD_BODY)) + 7) & ~7)
 
 #define PG_FIELD_BITS \
